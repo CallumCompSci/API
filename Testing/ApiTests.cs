65dc16a2a05d0c1dea380923c77132762fd52153
Callum Ritchie <callum.ritchie11@gmail.com>
2025-09-29 14:32:38 +0200

    Add integration tests for API startup

--- Testing/ApiTests.cs
+++ Testing/ApiTests.cs
@@ -1,1 +1,23 @@
-x = 10;
+using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace Last.Api.Tests
+{
+    public class ApiIntegrationTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+
+        public ApiIntegrationTests(WebApplicationFactory<Program> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Api_Starts_Successfully()
+        {
+            var response = await _client.GetAsync("/");
+            Assert.True(response.IsSuccessStatusCode || response.StatusCode == HttpStatusCode.NotFound);
+        }
+    }
+}
