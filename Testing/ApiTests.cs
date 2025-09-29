94ef39de21567f0c920ae53c2cb9f1c1a8d39da3
Callum Ritchie <callum.ritchie11@gmail.com>
2025-09-29 15:58:04 +0200

    Update ApiTests.cs

--- Testing/ApiTests.cs
+++ Testing/ApiTests.cs
@@ -1,8 +1,17 @@
+using System.Net.Http.Headers;
+using System.Text;
+
 using System.Net;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
 using System.Text.Json;
 using Last_Api.Models;
+
+
+
+
+
+
 
 namespace Last.Api.Tests
 {
@@ -25,9 +34,18 @@ namespace Last.Api.Tests
             Assert.True(response.IsSuccessStatusCode || response.StatusCode == HttpStatusCode.NotFound);
         }
 
+
+
+
         [Fact]
         public async Task Artifacts_returns()
         {
+            var username = "StephenB@gmail.com";
+            var password = "sit331password";
+            var authToken = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{username}:{password}"));
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authToken);
+    
+            
             var response = await _client.GetAsync("/api/artifacts");
 
             response.EnsureSuccessStatusCode();
