e7632c43e5948a6bc8c48a3bce60845e197adbb7
Callum Ritchie <callum.ritchie11@gmail.com>
2025-09-30 10:36:39 +0200

    Update ApiTests.cs

--- Testing/ApiTests.cs
+++ Testing/ApiTests.cs
@@ -26,7 +26,7 @@ namespace Last.Api.Tests
                 builder.UseContentRoot(Directory.GetCurrentDirectory());
             }).CreateClient();
 
-            var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9lbWFpbGFkZHJlc3MiOiJIYXJyeUBnbWFpbC5jb20iLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJhZG1pbiIsImV4cCI6MTc1OTE2NDg3MywiaXNzIjoiQ2FsbHVtc0FQSSIsImF1ZCI6IkFQSVVzZXIifQ.cRRW9TdBWwuli88I75HLZM9Hd88GStQldLlIMqxpRww"; 
+            var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9lbWFpbGFkZHJlc3MiOiJIYXJyeUBnbWFpbC5jb20iLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJhZG1pbiIsImV4cCI6MTc1OTIyMzE4MSwiaXNzIjoiQ2FsbHVtc0FQSSIsImF1ZCI6IkFQSVVzZXIifQ.zFsK6ahmbBa-8F-rYJKhavH4WU9sDOn98AQxg-w7k1s";
             _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
         }
 
